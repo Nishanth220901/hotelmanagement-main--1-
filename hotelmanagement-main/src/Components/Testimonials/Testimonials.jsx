4ed11645a046f4b84a3f00a2e7c1ae21708fe477
Nishanth220901 <nishanthrajendran220901@gmailcom>
2024-05-06 03:20:18 +0530

    testmonials

--- hotelmanagement-main/src/Components/Testimonials/Testimonials.jsx
+++ hotelmanagement-main/src/Components/Testimonials/Testimonials.jsx
@@ -10,13 +10,13 @@ function Testimonials() {
   const testimonialData = [
     {
       id: 1,
-      name: 'John Doe',
-      review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      name: 'Vanmuhil',
+      review: "I recently had the pleasure of staying at [Hotel Name] during a business trip, and I was blown away by the level of service and luxury. The location was convenient, the room was spacious and well-appointed, and the amenities were top-notch. The staff was incredibly friendly and accommodating, making me feel right at home. I wouldn't hesitate to recommend this hotel to anyone looking for a five-star experience.",
     },
     {
       id: 2,
-      name: 'Jane Smith',
-      review: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      name: 'Sandy',
+      review: "Our family vacation at H-Residency was nothing short of amazing! The kids had a blast at the pool and participating in the hotel's daily activities, while my husband and I enjoyed unwinding at the spa and exploring the local area. The room was comfortable and spacious, and the on-site dining options were delicious. The staff went out of their way to make us feel welcome, and we're already planning our next visit!",
     },
   
   ];
